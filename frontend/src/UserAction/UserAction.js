7b8f94231743dff68c1a8bc60e1b7eedf24fad6e
sidhant <sidhantnahak2@gmail.com>
2023-05-29 18:24:10 +0530

    some changes added

--- frontend/src/UserAction/UserAction.js
+++ frontend/src/UserAction/UserAction.js
@@ -11,46 +11,46 @@ import {
     deletenote_sucess,
     getuser_fail,
     getuser_request,
-     getuser_sucess,
-     login_fail,
-      login_request,
-       login_sucess,
-       logout_fail,
-       logout_request,
-       logout_sucess,
-       register_fail,
-      register_request,
-       register_sucess,
-       update_fail,
-       update_request,
-       update_sucess,
-     } from "../Constants/Constants";
+    getuser_sucess,
+    login_fail,
+    login_request,
+    login_sucess,
+    logout_fail,
+    logout_request,
+    logout_sucess,
+    register_fail,
+    register_request,
+    register_sucess,
+    update_fail,
+    update_request,
+    update_sucess,
+} from "../Constants/Constants";
 import axios from 'axios'
 
 
 axios.create({
-//     // baseURL: process.env.BASE_URL,
-// baseURL:  "https://note-making-app.onrender.com",
-   
-    baseURL: 'http://localhost:4000',
-    withCredentials:true
+    //     // baseURL: process.env.BASE_URL,
+    // baseURL: 'http://localhost:4000',
+
+    baseURL: "https://note-making-app.onrender.com",
+    withCredentials: true
 })
 
 
 
-export const register = (name,email, password,cpassword) => async (dispatch) => {
+export const register = (name, email, password, cpassword) => async (dispatch) => {
     try {
 
         dispatch({ type: register_request });
         // const config = { headers: {"Content-Type": "application/json"}};
         const { data } = await axios.post('/api/v1/register',
-            {name, email, password,cpassword }
+            { name, email, password, cpassword }
         );
 
         dispatch({ type: register_sucess, payload: data.user });
 
     } catch (error) {
-       
+
         dispatch({ type: register_fail, payload: error.response.data.message })
 
     }
@@ -76,91 +76,91 @@ export const login = (email, password) => async (dispatch) => {
 }
 
 
-export const Logout=()=>async(dispatch)=>{
+export const Logout = () => async (dispatch) => {
     try {
         dispatch({ type: logout_request });
-        
+
         const { data } = await axios.get('/api/v1/logout')
-        dispatch({ type: logout_sucess,payload:data.message});
+        dispatch({ type: logout_sucess, payload: data.message });
 
     } catch (error) {
-        dispatch({ type: logout_fail,payload:error.response.data.message });
-        
+        dispatch({ type: logout_fail, payload: error.response.data.message });
+
     }
 
 }
 
-export const getUser=()=>async(dispatch)=>{
+export const getUser = () => async (dispatch) => {
     try {
         dispatch({ type: getuser_request });
-        
+
         const { data } = await axios.post('/api/v1/me')
 
-        dispatch({ type: getuser_sucess,payload:data.user});
+        dispatch({ type: getuser_sucess, payload: data.user });
 
     } catch (error) {
-        dispatch({ type: getuser_fail,payload:error.response.data.message });
-        
+        dispatch({ type: getuser_fail, payload: error.response.data.message });
+
     }
 
 }
 
-export const getAllnotes=()=>async(dispatch)=>{
+export const getAllnotes = () => async (dispatch) => {
     try {
-        dispatch({ type:allnotes_request  });
-        
+        dispatch({ type: allnotes_request });
+
         const { data } = await axios.get('/api/v1/fetchallnotes')
-        dispatch({ type: allnotes_sucess,payload:data.notes});
+        dispatch({ type: allnotes_sucess, payload: data.notes });
 
     } catch (error) {
-        dispatch({ type: allnotes_fail,payload:error.response.data.message });
-        
+        dispatch({ type: allnotes_fail, payload: error.response.data.message });
+
     }
 }
 
-export const deletenote=(id)=>async(dispatch)=>{
+export const deletenote = (id) => async (dispatch) => {
     try {
-        dispatch({ type:deletenote_request  });
-        
+        dispatch({ type: deletenote_request });
+
         const { data } = await axios.delete(`/api/v1/deletenote/${id}`)
-        dispatch({ type: deletenote_sucess,payload:data.sucess});
+        dispatch({ type: deletenote_sucess, payload: data.sucess });
 
     } catch (error) {
-        dispatch({ type: deletenote_fail,payload:error.response.data.message });
-        
+        dispatch({ type: deletenote_fail, payload: error.response.data.message });
+
     }
 }
 
-export const addnote=(title,description)=>async(dispatch)=>{
+export const addnote = (title, description) => async (dispatch) => {
     try {
 
-        dispatch({ type:addnote_request  });
-        const config = { headers: {"Content-Type": "application/json"}};
-        
+        dispatch({ type: addnote_request });
+        const config = { headers: { "Content-Type": "application/json" } };
+
         const { data } = await axios.post(`/api/v1/createnote`,
-        {title,description},config
+            { title, description }, config
         )
-        dispatch({ type: addnote_sucess,payload:data.notes});
+        dispatch({ type: addnote_sucess, payload: data.notes });
 
     } catch (error) {
-        dispatch({ type: addnote_fail,payload:error.response.data.message });
-        
+        dispatch({ type: addnote_fail, payload: error.response.data.message });
+
     }
 }
 
-export const updatenote=(id,title,description)=>async(dispatch)=>{
+export const updatenote = (id, title, description) => async (dispatch) => {
     try {
 
-        dispatch({ type:update_request  });
-        const config = { headers: {"Content-Type": "application/json"}};
-        
+        dispatch({ type: update_request });
+        const config = { headers: { "Content-Type": "application/json" } };
+
         const { data } = await axios.put(`/api/v1/updatenote/${id}`,
-        {title,description},config
+            { title, description }, config
         )
-        dispatch({ type:update_sucess,payload:data.notes});
+        dispatch({ type: update_sucess, payload: data.notes });
 
     } catch (error) {
-        dispatch({ type: update_fail,payload:error.response.data.message });
-        
+        dispatch({ type: update_fail, payload: error.response.data.message });
+
     }
 }
