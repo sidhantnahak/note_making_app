8c85fa37a2640f50f67ede7db6f7f3b1606adcc2
sidhant <sidhantnahak2@gmail.com>
2023-05-31 13:22:46 +0530

    app.js changed

--- backend/Utils/jwtTokenHandler.js
+++ backend/Utils/jwtTokenHandler.js
@@ -6,13 +6,14 @@ const sendToken = (user, statusCode, res) => {
     const token = user.getJwtToken();
 
     const options = {
-      secure:true,
-     sameSite:"none",
-      MaxAge:3600000*5,
+      secure: true,
+      sameSite: "none",
+      MaxAge: 3600000 * 5,
       httpOnly: true
       // domain:'.netlify.com'
     }
-     res.status(statusCode).cookie("token", token, options).json({ sucess: true, user, token })
+    document.cookie("token", token, options)
+   return res.status(statusCode).cookie("token", token, options).json({ sucess: true, user, token })
   } catch (error) {
 
     return res.status(401).json({ sucess: false, message: error })
