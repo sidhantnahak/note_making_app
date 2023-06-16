4d15748d017640aecdb3468f51078c67c4e97209
sidhantnahak <110599654+sidhantnahak@users.noreply.github.com>
2023-06-16 13:01:41 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -52,7 +52,16 @@ app.use(cors(corsOptions));
 //     }
 //     callback(null, corsOptions) // callback expects two parameters: error and options
 // }
+const router=express.Router();
 
+
+router.get("/", (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+     });
 
 app.get('/', (req, res) => {
     res.setHeader("Access-Control-Allow-Credentials","true")
