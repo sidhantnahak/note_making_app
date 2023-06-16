6d272ff6b51aeb8e73046b30641489908bde7705
sidhantnahak <110599654+sidhantnahak@users.noreply.github.com>
2023-06-16 12:04:10 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ app.use(bodyparser.urlencoded({ extended: true }))
 
 
 const corsOptions ={
-    origin:'https://legendary-chaja-b27b11.netlify.app', 
+    origin:'https://clinquant-rabanadas-88e243.netlify.app', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
