6d8b60a80c9b3d6d285562385805b959b078ad44
sidhantnahak <110599654+sidhantnahak@users.noreply.github.com>
2023-06-16 11:43:49 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -7,9 +7,16 @@ const cookieparser = require('cookie-parser')
 const bodyparser = require('body-parser')
 
 app.use(express.json())
-app.use(cors())
 app.use(cookieparser())
 app.use(bodyparser.urlencoded({ extended: true }))
+
+const cors = require('cors');
+const corsOptions ={
+    origin:'https://deluxe-crisp-c07ed6.netlify.app', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 // app.use("uploads",express.static("./uploads"))
 // app.use("/files",express.static,("./public/files"))
