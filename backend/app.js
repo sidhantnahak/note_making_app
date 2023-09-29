50033e201a320f5b23764fe5e725577c2d6db4d2
sidhant <sidhantnahak2@gmail.com>
2023-09-29 15:10:51 +0530

    app.js updated

--- backend/app.js
+++ backend/app.js
@@ -8,8 +8,16 @@ const bodyparser = require('body-parser')
 
 
 
-const corsOptions={
-  origin: "http://localhost:3000"
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true,
+  methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+  allowedHeaders: [
+    'Access-Control-Allow-Origin',
+    'Content-Type',
+    'Authorization',
+  ]
+
 }
 
 app.use(cors(corsOptions))
@@ -30,7 +38,18 @@ app.use(cors(corsOptions))
 // )
 
 
-
+// app.use(cors());
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Methods", "GET, HEAD, OPTIONS, POST, PUT,DELETE"
+//   );
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   next();
+// });
 
 app.use(express.json())
 app.use(cookieparser())
